a534665190834653597d5d5a65e9494fde721ee6
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2025-01-25 22:22:45 +0530

    Initial Commit for Fixify app

---
+++ backend/src/main/java/com/fixify/FixifyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fixify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixifyAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixifyAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fixify/FixifyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixifyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
